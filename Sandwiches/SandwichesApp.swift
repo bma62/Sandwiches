d2e56faec096386cf7becaed2ff2052833802768
Boyi Ma <boyima98@gmail.com>
2021-01-05 16:16:07 -0500

    Initial Commit

---
+++ Sandwiches/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Sandwiches
+//
+//  Created by Boyi Ma on 2021-01-05.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
